932cd6afce538460d906ee4c8509e4d39c979961
Chethan13032005 <achethan777@gmail.com>
2026-06-05 12:22:16 +0530

    file added

--- Typescript/CS_15/cs15.ts
+++ Typescript/CS_15/cs15.ts
@@ -20,7 +20,8 @@ type Assignment={
 
 type ReadonlyAssignment=Readonly<Assignment>;
 
-// task 3 : Given a type LearnerStats = { quizzes: number; videos: number; assignments: number; }, create a type StatsAsStrings that has the same keys, but all values are strings.
+// task 3 : Given a type LearnerStats = { quizzes: number; videos: number; assignments: number; }, 
+// create a type StatsAsStrings that has the same keys, but all values are strings.
 type LearnerStats={
     quizzes:number;
     videos:number;
